dd074ab10bdd871dbce7d40edac6230b4eee5f3c
tlmohren <tlmohren@nogmail.com>
2022-08-23 14:33:23 -0400

    add pip install

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,6 +1,11 @@
 
+
+sudo apt-get update
+sudo apt-get install -y python3-pip
+
 pip3 install git+ssh://git@github.com/MaimonLab/event_data_logging.git
 
 pip3 install alicat
 pip3 install pyserial
+pip3 install ruamel.yaml
 sudo adduser $USER dialout
