cc857ccc9c2cc427c31bd713da659512174708f4
tlmohren <tlmohren@nogmail.com>
2022-08-23 15:57:58 -0400

    add pypi pip install of event-data-logging

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,9 +1,8 @@
-
-
 sudo apt-get update
 sudo apt-get install -y python3-pip
 
-pip3 install git+ssh://git@github.com/MaimonLab/event_data_logging.git
+# pip3 install git+https://git@github.com/MaimonLab/event_data_logging.git
+pip3 install event-data-logging
 
 pip3 install alicat
 pip3 install pyserial
